72bb50d31f9a8547b93972ce8ec1443366595b3e
Halí V <halivelazquez@outlook.com>
2024-11-27 14:57:38 -0600

    chore: add vite https for dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa"
+import { defineConfig } from "vite"
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+	plugins: [
+		vue(),
+		basicSsl(),
+		VitePWA({
+			registerType: "prompt",
+			injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
 
-    manifest: {
-      name: 'return-visits',
-      short_name: 'return-visits',
-      description: 'Track all your return visits locally',
-      theme_color: '#7FB069',
-    },
+			manifest: {
+				name: "return-visits",
+				short_name: "return-visits",
+				description: "Track all your return visits locally",
+				theme_color: "#7FB069",
+			},
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+			devOptions: {
+				enabled: false,
+				navigateFallback: "index.html",
+				suppressWarnings: true,
+				type: "module",
+			},
+		}),
+	],
 })
