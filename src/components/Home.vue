0c605d5d1ac8c27d43b872ec7047798e2f108bb3
Halí V <halivelazquez@outlook.com>
2024-11-27 15:32:10 -0600

    feat: add title and icon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="h-dvh overflow-y-scroll">
-		<main class="max-w-prose mx-auto">Hola</main>
+	<div class="h-dvh overflow-y-scroll bg-lemon-50 text-rangoon-950">
+		<main class="max-w-prose mx-auto py-2 px-3">
+			<div class="flex gap-3 items-center">
+				<h1 class="text-3xl font-display">Revisitas</h1>
+
+				<img
+					src="/favicon.webp"
+					class="min-h-0 h-10"
+					alt="Libretita de colores"
+				/>
+			</div>
+		</main>
 	</div>
 </template>
