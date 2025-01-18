f5ed875464967d7ae43cd90604112d80ef2a1a0b
halivert <halivelazquez+github@gmail.com>
2025-01-18 16:50:17 +0000

    Deploying to gh-pages from @ halivert/return-visits@c95fa8e4a16734a63de51e926e1badad8d37364f 🚀

---
+++ assets/usePersonReturnVisits-CEAQvDAy.js
@@ -0,0 +1,1 @@
+import{x as e,S as u}from"./index-B_PemqxQ.js";function n({personId:r,options:s}){return e(u(r,s))}export{n as u};
---
+++ assets/useReturnVisits-DjPXPxXm.js
@@ -0,0 +1,1 @@
+import{x as e,r,a4 as s}from"./index-B_PemqxQ.js";function u(){return e({queryKey:r.all(),queryFn:()=>s("returnVisits"),staleTime:0})}export{u};
